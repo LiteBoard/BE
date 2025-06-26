30ad771c912324439ecb536469641706f1d6bb22
shash0423 <shash042319@naver.com>
2025-06-26 11:13:22 +0900

    Initial commit

---
+++ src/main/java/we/LiteBoard/LiteBoardApplication.java
@@ -0,0 +1,13 @@
+package we.LiteBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteBoardApplication.class, args);
+	}
+
+}
