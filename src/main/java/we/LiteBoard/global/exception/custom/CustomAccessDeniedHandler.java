ec3bb79add36de0988bde5e489653ff4edd25b2a
shash0423 <shash042319@naver.com>
2025-06-27 09:25:15 +0900

    feat: 응답 규격화 & 에러 핸들링 #2

---
+++ src/main/java/we/LiteBoard/global/exception/custom/CustomAccessDeniedHandler.java
@@ -0,0 +1,30 @@
+package we.LiteBoard.global.exception.custom;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+import we.LiteBoard.global.exception.ErrorCode;
+import we.LiteBoard.global.response.ErrorResponse;
+
+import java.io.IOException;
+
+@Component
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response,
+                       AccessDeniedException accessDeniedException) throws IOException {
+        ErrorResponse<Object> errorResponse = ErrorResponse.of(
+                ErrorCode.FORBIDDEN.getErrorCode(),
+                ErrorCode.FORBIDDEN.getMessage()
+        );
+
+        response.setContentType("application/json;charset=UTF-8");
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
+    }
+}
---
+++ src/main/java/we/LiteBoard/global/exception/custom/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,26 @@
+package we.LiteBoard.global.exception.custom;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+import we.LiteBoard.global.exception.ErrorCode;
+import we.LiteBoard.global.response.ErrorResponse;
+
+import java.io.IOException;
+
+@Component
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
+        ErrorResponse<Object> errorResponse = ErrorResponse.of(ErrorCode.FORBIDDEN.getErrorCode(), ErrorCode.FORBIDDEN.getMessage());
+        response.setContentType("application/json;charset=UTF-8");
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
+    }
+}
