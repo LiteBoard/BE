867b51404f5f498331848f0d4f64fd971e5ea6f9
shash0423 <shash042319@naver.com>
2025-06-30 17:08:32 +0900

    chore: 불필요한 ErrorCode 삭제 #2

--- src/main/java/we/LiteBoard/global/exception/ErrorCode.java
+++ src/main/java/we/LiteBoard/global/exception/ErrorCode.java
@@ -57,99 +57,15 @@ public enum ErrorCode {
     USER_NO_PERMISSION("USER403", "권한이 없습니다.", HttpStatus.FORBIDDEN),
     USER_FORBIDDEN("USER403", "유저의 권한이 부족합니다.", HttpStatus.FORBIDDEN),
 
-    // UserOrganization
-    DUPLICATE_USER_ORGANIZATION("USER_ORGANIZATION400", "이미 조직에 가입된 사용자입니다.", HttpStatus.BAD_REQUEST),
-    USER_ORGANIZATION_NOT_FOUND("USER_ORGANIZATION404", "해당 조직에서 사용자를 찾을 수 없습니다.", HttpStatus.BAD_REQUEST),
-
-    // Organization (조직)
-    ORGANIZATION_ALREADY_EXIST("ORGANIZATION400", "이미 존재하는 조직입니다.", HttpStatus.BAD_REQUEST),
-    ORGANIZATION_NOT_EXIST("ORGANIZATION404", "존재하지 않는 조직입니다.", HttpStatus.NOT_FOUND),
-    USER_NOT_IN_ORGANIZATION("ORGANIZATION404", "조직에 속해있지 않은 유저입니다.", HttpStatus.NOT_FOUND),
-
-    // Recruitment (공고)
-    RECRUITMENT_ALREADY_EXIST("RECRUITMENT400", "이미 존재하는 공고입니다.", HttpStatus.BAD_REQUEST),
-    RECRUITMENT_NOT_EXIST("RECRUITMENT404", "존재하지 않는 공고입니다.", HttpStatus.NOT_FOUND),
-    SLUG_GENERATION_FAILED("RECRUITMENT500", "슬러그 생성에 실패하였습니다. 슬러그 자릿수를 늘리세요.", HttpStatus.INTERNAL_SERVER_ERROR),
-
-    // Available Time (면접 가능 시간)
-    AVAILABLE_TIME_NOT_EXIST("AVAILABLE_TIME404", "해당 날짜의 availableTimeRange가 없습니다.", HttpStatus.NOT_FOUND),
-
-    // DocumentQuestion (지원서 서류 질문)
-    DOCUMENT_QUESTION_ALREADY_EXIST("DOCUMENT_QUESTION400", "이미 존재하는 지원서 질문입니다.", HttpStatus.BAD_REQUEST),
-    DOCUMENT_QUESTION_NOT_EXIST("DOCUMENT_QUESTION404", "존재하지 않는 지원서 질문입니다.", HttpStatus.NOT_FOUND),
-
-    // Position (파트)
-    POSITION_ALREADY_EXIST("POSITION400", "이미 해당 파트가 존재합니다.", HttpStatus.BAD_REQUEST),
-    POSITION_NOT_EXIST("POSITION404", "존재하지 않는 파트입니다.", HttpStatus.NOT_FOUND),
-
-    // Application (지원서)
-    APPLICATION_ALREADY_EXIST("APPLICATION400", "이미 존재하는 지원서입니다.", HttpStatus.BAD_REQUEST),
-    APPLICATION_NOT_EXIST("APPLICATION404", "존재하지 않는 지원서입니다.", HttpStatus.NOT_FOUND),
-    REQUIRED_FIELD_MISSING("APPLICATION400", "필수 입력값이 누락되었습니다.", HttpStatus.BAD_REQUEST),
-    STAGE_NOT_SUPPORTED("APPLICATION400", "해당 단계에서는 상태 변경을 지원하지 않습니다.", HttpStatus.BAD_REQUEST),
-
-    // 평가자 관련
-    INSUFFICIENT_EVALUATORS("APPLICATION400", "해당 파트에 평가자가 충분하지 않습니다.", HttpStatus.BAD_REQUEST),
-    EVALUATOR_NOT_EXIST("APPLICATION404", "존재하지 않는 평가자입니다.", HttpStatus.NOT_FOUND),
-    DISTRIBUTION_HISTORY_NOT_FOUND("APPLICATION404", "해당 공고에 대한 분배 요청 이력이 없습니다.", HttpStatus.NOT_FOUND),
-
-    // Interview (면접)
-    INTERVIEW_ALREADY_EXIST("INTERVIEW400", "이미 존재하는 면접입니다.", HttpStatus.BAD_REQUEST),
-    INTERVIEW_NOT_EXIST("INTERVIEW404", "존재하지 않는 면접입니다.", HttpStatus.NOT_FOUND),
-    MISMATCHED_ROOM_COUNT("INTERVIEW400", "roomNames 수가 roomCount와 일치하지 않습니다.",HttpStatus.BAD_REQUEST),
-    INTERVIEW_QUESTION_NOT_EXIST("INTERVIEW_QUESTION400", "존재하지 않는 면접 질문입니다.", HttpStatus.NOT_FOUND),
-
-    // TimeSlot (면접)
-    TIME_SLOT_ALREADY_EXIST("TIME_SLOT400", "이미 존재하는 TimeSlot입니다.", HttpStatus.BAD_REQUEST),
-    TIME_SLOT_NOT_EXIST("TIME_SLOT404", "존재하지 않는 TimeSlot입니다.", HttpStatus.NOT_FOUND),
-
-    // EvaluationCriteria (평가 기준)
-    EVALUATION_CRITERIA_ALREADY_EXIST("EVALUATION_CRITERIA400", "이미 존재하는 평가 기준입니다.", HttpStatus.BAD_REQUEST),
-    EVALUATION_CRITERIA_NOT_EXIST("EVALUATION_CRITERIA404", "존재하지 않는 평가 기준입니다.", HttpStatus.NOT_FOUND),
-
-    // Evaluation (평가)
-    EVALUATION_ALREADY_EXIST("EVALUATION400", "이미 존재하는 평가입니다.", HttpStatus.BAD_REQUEST),
-    EVALUATION_NOT_EXIST("EVALUATION404", "존재하지 않는 평가입니다.", HttpStatus.NOT_FOUND),
-
-    // Role
-    INVALID_ROLE("ROLE400", "잘못된 Role 값입니다.", HttpStatus.BAD_REQUEST),
-
-    // OrganizationRole (조직 역할)
-    ORGANIZATION_ROLE_ALREADY_EXIST("ORGANIZATION_ROLE400", "이미 추가된 역할입니다.", HttpStatus.BAD_REQUEST),
-    DUPLICATE_ORGANIZATION_ROLE_NAME("ORGANIZATION_ROLE400", "이미 존재하는 역할명입니다.", HttpStatus.BAD_REQUEST),
-    ORGANIZATION_ROLE_NOT_EXIST("ORGANIZATION_ROLE404", "등록되지 않은 조직 역할입니다.", HttpStatus.NOT_FOUND),
-    ORGANIZATION_ROLE_ORG_MISMATCH("ORGANIZATION_ROLE403", "해당 역할은 해당 조직에 속해 있지 않습니다.", HttpStatus.FORBIDDEN),
-
-    // Comment
-    COMMENT_NOT_EXIST("COMMENT404", "존재하지 않는 코멘트입니다.", HttpStatus.NOT_FOUND),
-
-    // Verification
-    VERIFICATION_EXPIRED("VERIFICATION404", "인증 코드가 만료되었습니다.", HttpStatus.NOT_FOUND),
-    VERIFICATION_INVALID("VERIFICATION401", "유효하지 않은 인증 코드입니다.", HttpStatus.UNAUTHORIZED),
-    VERIFICATION_NOT_EQUAL("VERIFICATION401", "인증 코드가 일차하지 않습니다.", HttpStatus.UNAUTHORIZED),
-    NOT_VERIFIED("VERIFICATION403", "인증이 완료되지 않았습니다.", HttpStatus.FORBIDDEN),
-    EMAIL_AUTH_FAIL("EMAIL401", "이메일 인증에 실패했습니다.", HttpStatus.UNAUTHORIZED),
-    EMAIL_REQUEST_LIMIT_EXCEEDED("EMAIL429", "5분 후 다시 시도해주세요.", HttpStatus.TOO_MANY_REQUESTS),
-
-    // TEMPLATE
-    TEMPLATE_NOT_FOUND("TEMPLATE404", "존재하지 않는 템플릿입니다.", HttpStatus.NOT_FOUND),
-    EMAIL_SUBJECT_REQUIRED("TEMPLATE402", "이메일 템플릿의 경우 제목이 필요합니다.", HttpStatus.BAD_REQUEST),
-
     // EMAIL
     EMAIL_SEND_FAIL("EMAIL500", "메일 전송에 실패하였습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
     TEMPLATE_NOT_LOAD("EMAIL500", "이메일 템플릿을 로드할 수 없습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
 
-    // SMS
-    SMS_SEND_FAIL("SMS500", "문자 발송에 실패하였습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
-    SMS_ATTACHMENT_PROCESS_FAIL("SMS500", "문자 첨부파일 처리에 실패하였습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
-
     // File Upload
     FILE_UPLOAD_FAIL("FILE500", "파일 업로드에 실패했습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
     FILE_IO_ERROR("FILE500", "파일 처리 중 I/O 오류가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
-    INVALID_URL("FILE400", "잘못된 이미지 URL 형식입니다", HttpStatus.BAD_REQUEST),
-    FILE_DELETE_FAIL("FILE500", "파일 삭제에 실패하였습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
-    FILE_NAME_NOT_MATCH("FILE400", "요청한 파일명이 업로드된 파일과 일치하지 않습니다.", HttpStatus.BAD_REQUEST),
-    FILE_COUNT_MISMATCH("FILE400", "요청한 파일 수와 실제 업로드된 파일 수가 일치하지 않습니다.", HttpStatus.BAD_REQUEST);
+    FILE_INVALID_URL("FILE400", "잘못된 이미지 URL 형식입니다", HttpStatus.BAD_REQUEST),
+    FILE_DELETE_FAIL("FILE500", "파일 삭제에 실패하였습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
 
     private final String errorCode;
     private final String message;
