1ddc7aa46e73b7098fac3aafb309be57e885125a
shash0423 <shash042319@naver.com>
2025-07-06 15:52:53 +0900

    refactor: 업무 응답에 TODO, 업무 요청 추가 #11

--- src/main/java/we/LiteBoard/domain/task/dto/TaskResponseDTO.java
+++ src/main/java/we/LiteBoard/domain/task/dto/TaskResponseDTO.java
@@ -2,11 +2,14 @@ package we.LiteBoard.domain.task.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import we.LiteBoard.domain.member.dto.MemberResponseDTO;
+import we.LiteBoard.domain.requestCard.dto.RequestCardResponseDTO;
 import we.LiteBoard.domain.task.entity.Task;
 import we.LiteBoard.domain.task.enumerate.Status;
+import we.LiteBoard.domain.todo.dto.TodoResponseDTO;
 import we.LiteBoard.global.common.annotation.DateFormat;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Schema(description = "업무 관련 응답 DTO")
 public class TaskResponseDTO {
@@ -30,7 +33,9 @@ public class TaskResponseDTO {
             @Schema(description = "상태") Status status,
             @Schema(description = "시작일") @DateFormat LocalDate startDate,
             @Schema(description = "마감일") @DateFormat LocalDate endDate,
-            @Schema(description = "담당자 정보") MemberResponseDTO.Summary member
+            @Schema(description = "담당자 정보") MemberResponseDTO.Summary member,
+            @Schema(description = "TODO 목록") List<TodoResponseDTO.Detail> todos,
+            @Schema(description = "업무 요청 목록") List<RequestCardResponseDTO.Detail> requestCards
     ) {
         public static TaskResponseDTO.Detail from(Task task) {
             return new TaskResponseDTO.Detail(
@@ -40,7 +45,13 @@ public class TaskResponseDTO {
                     task.getStatus(),
                     task.getEndDate(),
                     task.getEndDate(),
-                    MemberResponseDTO.Summary.from(task.getMember())
+                    MemberResponseDTO.Summary.from(task.getMember()),
+                    task.getTodos().stream()
+                            .map(TodoResponseDTO.Detail::from)
+                            .toList(),
+                    task.getRequestCards().stream()
+                            .map(RequestCardResponseDTO.Detail::from)
+                            .toList()
             );
         }
     }
