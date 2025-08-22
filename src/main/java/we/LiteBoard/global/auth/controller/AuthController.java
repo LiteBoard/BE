b88a2a2f5482285b7a92005889b2fd33ce7b10b6
shash0423 <shash042319@naver.com>
2025-08-22 15:29:35 +0900

    feat: 토급 발급시 유저 정보 반환 #1

--- src/main/java/we/LiteBoard/global/auth/controller/AuthController.java
+++ src/main/java/we/LiteBoard/global/auth/controller/AuthController.java
@@ -6,6 +6,9 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import we.LiteBoard.domain.member.dto.MemberResponseDTO;
+import we.LiteBoard.domain.member.entity.Member;
+import we.LiteBoard.domain.member.repository.MemberRepository;
 import we.LiteBoard.global.auth.jwt.util.JWTUtil;
 import we.LiteBoard.global.auth.service.AuthService;
 import we.LiteBoard.global.util.redis.TokenCache;
@@ -18,10 +21,11 @@ public class AuthController {
     private final JWTUtil jwtUtil;
     private final AuthService authService;
     private final TokenCache tokenCache;
+    private final MemberRepository memberRepository;
 
     @GetMapping("/token")
     @Operation(summary = "토큰 발급", description = "쿠키에 담긴 Refresh Token을 꺼내 응답 헤더로 내려줍니다.")
-    public ResponseEntity<Void> issueTokensInHeader(
+    public ResponseEntity<MemberResponseDTO.Info> issueTokensInHeader(
             @CookieValue(name = "Refresh-Token", required = false) String refreshToken
     ) {
         if (refreshToken == null || jwtUtil.isExpired(refreshToken)) {
@@ -41,12 +45,18 @@ public class AuthController {
         String role = jwtUtil.getRole(refreshToken);
         String newAccessToken = jwtUtil.createAccessToken(id, email, role);
 
+        // Member 정보 바디에 담아서 반환
+        Member member = memberRepository.getById(id);
+        MemberResponseDTO.Info info = MemberResponseDTO.Info.from(member);
+
         // 헤더에 토큰 담아서 응답
         HttpHeaders headers = new HttpHeaders();
         headers.set(HttpHeaders.AUTHORIZATION, "Bearer " + newAccessToken);
         headers.set("Refresh-Token", refreshToken);
 
-        return ResponseEntity.ok().headers(headers).build();
+        return ResponseEntity.ok()
+                .headers(headers)
+                .body(info);
     }
 
     @PostMapping("/reissue")
